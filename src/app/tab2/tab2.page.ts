f24b2b8d99f434ceda7cf973332e24b247370254
EliasInfnet <elias@uakti.com>
2021-11-16 11:03:31 -0300

    2 commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  valorSelect = '';
+  valorSelect = '1';
 
   constructor() {}
 
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,8 +8,7 @@ import { Component } from '@angular/core';
 export class Tab3Page {
   valorRange = 0;
 
-  imagem = 'https://media.istockphoto.com/photos/rear-view-of-a-mans-bald-head-picture-id147486873?'
-  +'k=20&m=147486873&s=612x612&w=0&h=tK77FEHbuOq5z_BUrBnPL4iPJmA1TG8tke7VnNr5lpI=';
+  imagem = 'https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55893/angry-face-emoji-clipart-md.png';
 
   mostrarPrimeiraFace = true;
   mostrarSegundaFace = false;
@@ -19,15 +18,13 @@ export class Tab3Page {
 
   onRangeChange(e){
     if(e.target.value >= 0 && e.target.value < 80){
-      this.imagem = 'https://media.istockphoto.com/photos/rear-view-of-a-mans-bald-head-picture-id147486873?'+
-      'k=20&m=147486873&s=612x612&w=0&h=tK77FEHbuOq5z_BUrBnPL4iPJmA1TG8tke7VnNr5lpI=';
+      this.imagem = 'https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55893/angry-face-emoji-clipart-md.png';
     }
     else if(e.target.value >= 80 && e.target.value < 160){
-      this.imagem = 'https://simg.nicepng.com/png/small/60-603127_slow-stop-and-reverse-your-hair-loss-bald.png';
+      this.imagem = 'https://www.pngkit.com/png/full/57-576420_poker-face-meme-png-meme-faces-poker-face.png';
     }
     else {
-      this.imagem = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO5Sh9-'
-      +'AWOS3fqDVBu6JMuMzgQvsXe_yMQmiwG7R0On5OA3ds3kGeoCy_S_HzBZ6X5Yw0&usqp=CAU';
+      this.imagem = 'https://www.freeiconspng.com/uploads/happy-png-14.png';
     }
   }
 }
